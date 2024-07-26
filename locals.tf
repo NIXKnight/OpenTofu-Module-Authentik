298a00753d5bb3ee83973415c2bb395ac30992ba
Saad Ali <engr.saadali786@gmail.com>
2024-07-26 19:16:39 +0500

    Added generic LDAP provider

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
   authentik_config = yamldecode(file("${var.authentik_config_file}"))
+  enable_generic_ldap_provider = lookup(local.authentik_config, "enable_generic_ldap_provider", false)
+  ldap_provider_base_dn = lookup(local.authentik_config, "ldap_provider_base_dn", "dc=ldap,dc=goauthentik,dc=io")
 }
--- main.tf
+++ main.tf
@@ -6,11 +6,79 @@ data "authentik_certificate_key_pair" "default" {
   name = "authentik Self-signed Certificate"
 }
 
+# LDAP generic provider setup, as per Authentik documentation https://docs.goauthentik.io/docs/providers/ldap/generic_setup
+resource "authentik_stage_user_login" "ldap_generic" {
+  count = local.enable_generic_ldap_provider ? 1 : 0
+  name  = "ldap-generic-user-login"
+}
+
+resource "authentik_stage_password" "ldap_generic" {
+  count = local.enable_generic_ldap_provider ? 1 : 0
+  name     = "ldap-generic-password"
+  backends = [
+    "authentik.core.auth.TokenBackend",
+    "authentik.core.auth.InbuiltBackend",
+    "authentik.sources.ldap.auth.LDAPBackend"
+  ]
+}
+
+resource "authentik_stage_identification" "ldap_generic" {
+  count          = local.enable_generic_ldap_provider ? 1 : 0
+  name           = "ldap-generic-identification"
+  user_fields    = ["username", "email"]
+  password_stage = authentik_stage_password.ldap_generic[0].id
+}
+
+resource "authentik_flow" "ldap_generic" {
+  count       = local.enable_generic_ldap_provider ? 1 : 0
+  name        = "ldap-generic-flow"
+  title       = "ldap-generic-flow"
+  slug        = "ldap-generic-flow"
+  designation = "authentication"
+}
+
+resource "authentik_flow_stage_binding" "ldap_generic_identification" {
+  count  = local.enable_generic_ldap_provider ? 1 : 0
+  stage  = authentik_stage_identification.ldap_generic[0].id
+  target = authentik_flow.ldap_generic[0].uuid
+  order  = 10
+}
+
+resource "authentik_flow_stage_binding" "ldap_generic_login" {
+  count  = local.enable_generic_ldap_provider ? 1 : 0
+  stage  = authentik_stage_user_login.ldap_generic[0].id
+  target = authentik_flow.ldap_generic[0].uuid
+  order  = 30
+}
+
+resource "authentik_provider_ldap" "generic" {
+  count   = local.enable_generic_ldap_provider ? 1 : 0
+  name    = "ldap-generic-provider"
+  base_dn = local.ldap_provider_base_dn
+  bind_flow = authentik_flow.ldap_generic[0].uuid
+}
+
+resource "authentik_application" "ldap_generic" {
+  count            = local.enable_generic_ldap_provider ? 1 : 0
+  name             = "ldap-generic"
+  slug             = "ldap-generic"
+  protocol_provider = authentik_provider_ldap.generic[0].id
+}
+
+resource "authentik_outpost" "ldap_generic" {
+  count              = local.enable_generic_ldap_provider ? 1 : 0
+  name               = "ldap-generic"
+  type               = "ldap"
+  protocol_providers = [authentik_provider_ldap.generic[0].id]
+}
+
+# Scope mapping
 data "authentik_scope_mapping" "scope_mappings" {
   for_each = { for mapping in local.authentik_config.scope_mappings : mapping.name => mapping }
   managed_list = each.value.managed_list
 }
 
+# OAuth2 provider and application
 resource "authentik_provider_oauth2" "oauth2_providers" {
   depends_on = [ data.authentik_scope_mapping.scope_mappings ]
 
@@ -31,6 +99,7 @@ resource "authentik_application" "applications" {
   protocol_provider = authentik_provider_oauth2.oauth2_providers[each.value.provider].id
 }
 
+# Users and groups
 resource "authentik_user" "users" {
   for_each = { for user in local.authentik_config.users : user.username => user }
   username = each.value.username
